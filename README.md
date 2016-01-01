5e1fc6c357a2a8c8536225fc31492a653081bcf3
Charlie Somerville <charlie@charliesomerville.com>
2016-01-01 18:31:40 +1100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# BlockBlockAdBlock
+
+This Chrome extension blocks the [BlockAdBlock](https://github.com/sitexw/BlockAdBlock) script, because **you** have the right to decide what code runs on your computer.
+
---
+++ blockblockadblock.js
@@ -0,0 +1,16 @@
+function injected() {
+    var descriptor = {
+        value: {},
+        writeable: false,
+        configurable: false,
+    };
+
+    Object.defineProperty(window, "BlockAdBlock", descriptor);
+    Object.defineProperty(window, "blockAdBlock", descriptor);
+}
+
+var script = document.createElement("script");
+
+script.appendChild(document.createTextNode("(" + injected + ")();"));
+
+document.children[0].appendChild(script);
