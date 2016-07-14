1f9f5b303d57fe879c768256dc5f2397f114f22f
James Edward Lewis II <lewisje@users.noreply.github.com>
2016-07-13 22:20:38 -0400

    support TamperMonkey, don't rely on func->string

--- blockblockadblock.js
+++ blockblockadblock.js
@@ -1,16 +1,30 @@
-function injected() {
-    var descriptor = {
-        value: {},
-        writable: false,
-        configurable: false,
-    };
+// ==UserScript==
+// @author Charlie Somerville
+// @description This prevents the BlockAdBlock anti-Adblock script from working.
+// @name Block BlockAdBlock
+// @namespace greasyfork.org
+// @version 1.0.0
+// @icon https://adblockplus.org/favicon.ico
+// @include *
+// @license MIT
+// @grant none
+// @run-at document-start
+// @copyright 2016 Charlie Somerville
+// ==/UserScript==
 
-    Object.defineProperty(window, "BlockAdBlock", descriptor);
-    Object.defineProperty(window, "blockAdBlock", descriptor);
-}
+var injected = '(function () {\n' +
+'    var descriptor = {\n' +
+'        value: {},\n' +
+'        writable: false,\n' +
+'        configurable: false,\n' +
+'    };\n' +
+'\n'
+'    Object.defineProperty(window, "BlockAdBlock", descriptor);\n' +
+'    Object.defineProperty(window, "blockAdBlock", descriptor);\n' +
+'})();';
 
 var script = document.createElement("script");
 
-script.appendChild(document.createTextNode("(" + injected + ")();"));
+script.appendChild(document.createTextNode(injected);
 
 document.children[0].appendChild(script);
